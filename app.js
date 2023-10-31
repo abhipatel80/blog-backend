9cfb039f0556c454ea162adc2fd82f0668dea7fb
Abhi Patel <abhipatel6788@gmail.com>
2023-10-31 16:40:04 +0530

    cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dbConnection();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
-app.use(cors());
+app.use(cors({ origin: "https://blogify-me.vercel.app", credentials: true }));
 app.use("/blog", blog);
 app.use("/user", user);
 
