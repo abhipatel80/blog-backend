9da18df7502950f9d7f806377f53e81046f5bec4
Abhi Patel <abhipatel6788@gmail.com>
2023-11-06 13:14:29 +0530

    change file upload code

--- utils/upload.js
+++ utils/upload.js
@@ -1,44 +1,52 @@
-import multer from 'multer';
+import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/files/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    const dest = path.join(__dirname, "public/files/");
+    cb(null, dest);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 const upload = multer({
-    storage, limits: {
-        fileSize: 1024 * 1024 * 2
-    },
-    fileFilter,
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+  fileFilter,
 });
 
 const mystorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/userImages/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    const dest = path.join(__dirname, "public/userImages/");
+    cb(null, dest);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 export const userImgupload = multer({
-    storage: mystorage,
-    limits: {
-        fileSize: 1024 * 1024 * 2
-    },
-    fileFilter,
+  storage: mystorage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+  fileFilter,
 });
 
-export default upload
+export default upload;
